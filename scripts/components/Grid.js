99f5d5777c4de2517051dbfebc39c4d678bf354d
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-07-29 22:49:54 +0900

    feat: Grid

--- scripts/components/Grid.js
+++ scripts/components/Grid.js
@@ -1,10 +1,21 @@
 import { ExtendedObject3D } from "../utils/ExtendedObject3D";
+import { Card } from "./Card.js";
 
 export class Grid extends ExtendedObject3D {
   static COLUMNS = Math.floor(window.innerWidth / 100) | 1;
   static ROWS = Math.floor(window.innerHeight / 100) | 1;
   constructor() {
     super();
+    this.#_createCards();
+  }
+
+  #_createCards() {
+    for (let i = 0; i < Grid.COLUMNS; i++) {
+      for (let j = 0; j < Grid.ROWS; j++) {
+        const card = new Card(i, j);
+        this.add(card);
+      }
+    }
   }
 
   resize(event) {
