39842e5c7aa74c287f7d12c41152bd0764bca257
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-07-31 15:32:01 +0900

    feat: Card

--- scripts/components/Card.js
+++ scripts/components/Card.js
@@ -1,8 +1,30 @@
+import { Mesh, MeshBasicMaterial, PlaneGeometry, Vector2 } from "three";
 import { ExtendedObject3D } from "../utils/ExtendedObject3D";
+import * as THREE from "three";
 
 export class Card extends ExtendedObject3D {
-  constructor() {
+  static Geometry = new PlaneGeometry(1, 1);
+
+  gridPosition = new Vector2();
+  mesh;
+
+  constructor(i, j) {
     super();
+    this.gridPosition.set(i, j);
+    this.#_createMesh();
+  }
+
+  #_createMesh() {
+    const r = Math.ceil(Math.random() * 255);
+    const g = Math.ceil(Math.random() * 255);
+    const b = Math.ceil(Math.random() * 255);
+
+    this.mesh = new Mesh(
+      Card.Geometry,
+      new MeshBasicMaterial({ color: new THREE.Color(`rgb(${r}, ${g}, ${b})`) })
+    );
+
+    this.add(this.mesh);
   }
 
   resize(event) {}
